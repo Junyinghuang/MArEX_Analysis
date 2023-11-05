f4399a812b84de1f905b040f052ba45ee280374e
Junying Huang <xxzdkg@gmail.com>
2023-11-05 00:46:12 -0700

    update compress

--- data/compress.py
+++ data/compress.py
@@ -75,11 +75,64 @@ def compress_PTBC(run_number):
     PTBC = file["PTBC;1"]
     PKUP = file["PKUP;1"]
 
-    f = h5py.File("PTBC_" + run_number + ".hdf5", "w")
+    f = h5py.File("h5" + run_number + ".hdf5", "w")
 
     h5_PTBC = f.create_group("PTBC")
     data = PTBC
     save = h5_PTBC
+    tflash = data['tflash'].array(library="np")
+    tof = data['tof'].array(library="np")
+    detn = data['detn'].array(library="np")
+    BN = data['BunchNumber'].array(library="np")
+    PI = data['PulseIntensity'].array(library="np")
+    amp = data['amp'].array(library="np")
+    area = data['area'].array(library="np")
+    save.create_dataset("tflash", data=tflash)
+    save.create_dataset("tof", data=tof)
+    save.create_dataset("detn", data=detn)
+    save.create_dataset("BunchNumber", data=BN)
+    save.create_dataset("PulseIntensity", data=PI)
+    save.create_dataset("amp", data=amp)
+    save.create_dataset("area", data=area)
+
+    h5_PKUP = f.create_group("PKUP")
+    data = PKUP
+    save = h5_PKUP
+    tflash = data['tflash'].array(library="np")
+    save.create_dataset("tflash", data=tflash)
+
+    f.close()
+
+def compress_MGAS(run_number):
+    run_number = str(run_number)
+    file = uproot.open("run" + run_number + ".root")
+    PTBC = file["PTBC;1"]
+    PKUP = file["PKUP;1"]
+    FIMG = file["FIMG;1"]
+
+    f = h5py.File("h5" + run_number + ".hdf5", "w")
+
+    h5_PTBC = f.create_group("PTBC")
+    data = PTBC
+    save = h5_PTBC
+    tflash = data['tflash'].array(library="np")
+    tof = data['tof'].array(library="np")
+    detn = data['detn'].array(library="np")
+    BN = data['BunchNumber'].array(library="np")
+    PI = data['PulseIntensity'].array(library="np")
+    amp = data['amp'].array(library="np")
+    area = data['area'].array(library="np")
+    save.create_dataset("tflash", data=tflash)
+    save.create_dataset("tof", data=tof)
+    save.create_dataset("detn", data=detn)
+    save.create_dataset("BunchNumber", data=BN)
+    save.create_dataset("PulseIntensity", data=PI)
+    save.create_dataset("amp", data=amp)
+    save.create_dataset("area", data=area)
+
+    h5_FIMG = f.create_group("FIMG")
+    data = FIMG
+    save = h5_FIMG
     tflash = data['tflash'].array(library="np")
     tof = data['tof'].array(library="np")
     detn = data['detn'].array(library="np")
---
+++ src/process_h5_data.py
@@ -0,0 +1,53 @@
+import numpy as np
+import h5py
+
+def energy(tof, x, m, c):
+    return m * (1 / np.sqrt(1 - x * x / c / c / tof / tof) - 1)
+
+
+def get_data(run_number, detector, L):
+    run_number = str(run_number)
+    data = h5py.File("data/compressed/h5" + run_number + ".hdf5")[detector]
+    PKUP = h5py.File("data/compressed/h5" + run_number + ".hdf5")['PKUP']
+    PKUP_tflash = PKUP['tflash'][:]
+    # tflash = data['tflash'][:]
+    tof = data['tof'][:]
+    # detn = data['detn'][:]
+    BN = data['BunchNumber'][:]
+    PI = data['PulseIntensity'][:]
+    amp = data['amp'][:]
+    norm = PI[0]
+    for i in range(1, len(PI)):
+        if BN[i] != BN[i - 1]:
+            norm += PI[i]
+    real_tof = np.zeros(len(tof))
+    diff = 660
+    if detector == "FIMG":
+        diff = 630
+    for i in range(len(tof)):
+        real_tof[i] = tof[i] - (PKUP_tflash[BN[i] - 1] - diff - L / 299792458)
+    return real_tof, amp, norm
+
+
+def process_data(run_numbers, detector, output):
+    tof = np.array([])
+    amp = np.array([])
+    norm = 0
+    # L = 184.5
+    L = 182.24
+    if detector == "C6D6":
+        L += 6.89
+    if detector == "FIGM":
+        L += 80
+    for i in run_numbers:
+        tof_i, amp_i, norm_i = get_data(i, detector, L)
+        tof = np.append(tof, tof_i)
+        amp = np.append(amp, amp_i)
+        norm += norm_i
+    en = energy(tof / 1e9, L, 939.56542, 299792458) * 1e6  # eV
+    f = h5py.File(output, "w")
+    f.create_dataset("energy", data=en)
+    f.create_dataset("amp", data=amp)
+    f.create_dataset("norm", data=[norm])
+    f.create_dataset("tof", data=tof)
+    f.close()
